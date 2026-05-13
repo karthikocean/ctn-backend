60066e6fe945264c020344f52ea4d40bafa78dec
Thennarasu <thennarasu@oceansoftteam.in>
2026-05-13 09:45:15 +0530

    spotlight and businessregion training api's implemented

---
+++ src/entity/BusinessRegion.ts
@@ -0,0 +1,44 @@
+import {
+  Entity,
+  ObjectIdColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn
+} from "typeorm";
+import { ObjectId } from "mongodb";
+
+export enum BusinessRegionStatus {
+  ACTIVE = "active",
+  INACTIVE = "inactive"
+}
+
+@Entity("business_regions")
+export class BusinessRegion {
+  @ObjectIdColumn()
+    _id!: ObjectId;
+
+  @Column()
+    country!: string;
+
+  @Column()
+    state!: string;
+
+  @Column()
+    city!: string;
+
+  @Column({
+    type: "enum",
+    enum: BusinessRegionStatus,
+    default: BusinessRegionStatus.ACTIVE
+  })
+    status!: BusinessRegionStatus;
+
+  @Column({ default: false })
+    isDeleted!: boolean;
+
+  @CreateDateColumn()
+    createdAt!: Date;
+
+  @UpdateDateColumn()
+    updatedAt!: Date;
+}
