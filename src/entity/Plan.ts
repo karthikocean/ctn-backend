bcf0358d55f51e06e464a702f605b1e758cb8386
Thennarasuraja818 <thennarasu@oceansoftteam.in>
2026-05-16 17:12:58 +0530

    implement place apis

---
+++ src/entity/Plan.ts
@@ -0,0 +1,41 @@
+import {
+  Entity,
+  ObjectIdColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { ObjectId } from "mongodb";
+
+@Entity("plans")
+export class Plan {
+  @ObjectIdColumn()
+    _id!: ObjectId;
+
+  @Column()
+    title!: string;
+
+  @Column()
+    description!: string;
+
+  @Column()
+    amount!: number;
+
+  @Column({ default: "active" })
+    status!: string;
+
+  @Column("simple-json")
+    modules!: {
+    moduleName: string;
+    countLimit: number;
+  }[];
+
+  @Column({ default: false })
+    isDeleted!: boolean;
+
+  @CreateDateColumn()
+    createdAt!: Date;
+
+  @UpdateDateColumn()
+    updatedAt!: Date;
+}
