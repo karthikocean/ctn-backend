5304f12cb9b9e9aa18a6fd75f1c67d2ee449466e
Anbu-Elumalai <anbuelumalai952002@gmail.com>
2026-06-23 18:22:01 +0530

    working on issues

--- src/middlewares/FranchiseFilterMiddleware.ts
+++ src/middlewares/FranchiseFilterMiddleware.ts
@@ -6,7 +6,6 @@ import { ObjectId } from "mongodb";
 
 export const franchiseFilter = async (req: any, res: Response, next: NextFunction): Promise<void> => {
   try {
-    console.log(req.user?.role?.code, 'req.user?.role?.code')
     if (req.user?.role?.code?.toUpperCase() === "FRANCHISE_OWNER") {
       const franchiseRepo = AppDataSource.getMongoRepository(Franchise);
       const franchise = await franchiseRepo.findOne({
