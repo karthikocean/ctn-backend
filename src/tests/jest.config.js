7a68118045ee06092e7cb941fee213162fe9ee70
Anbu-Elumalai <anbuelumalai952002@gmail.com>
2026-07-22 09:44:56 +0530

    spent issue

--- src/tests/jest.config.js
+++ src/tests/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  rootDir: '../../',
-  testMatch: ['<rootDir>/src/tests/**/*.test.ts'],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  rootDir: "../../",
+  testMatch: ["<rootDir>/src/tests/**/*.test.ts"],
 };
