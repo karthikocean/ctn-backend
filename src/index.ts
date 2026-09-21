338abe00a8a76aee4b35529a82d790c4da127103
Anbu-Elumalai <anbuelumalai952002@gmail.com>
2026-09-21 10:07:57 +0530

    feat: add server entry point and admin dashboard controller

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,6 @@ import { AnniversaryCronService } from "./services/anniversaryCron.service";
 import { DailyTaskCronService } from "./services/dailyTaskCron.service";
 import { SpotlightRequestCronService } from "./services/spotlightRequestCron.service";
 import { MilestoneCronService } from "./services/milestoneCron.service";
-import { MemberInactivityCronService } from "./services/memberInactivityCron.service";
 import { DataRetentionCronService } from "./services/dataRetentionCron.service";
 import { ensureMongoIndexes } from "./utils/ensureIndexes";
 import { logger } from "./utils/logger";
